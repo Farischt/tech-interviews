4773b87dcde6c2ebc676fb717c3de2dfe35d2fae
Faris CHTATOU <faris.manage@gmail.com>
2023-01-02 19:48:51 +0100

    add container with most water

--- two_pointers/medium.go
+++ two_pointers/medium.go
@@ -1,5 +1,7 @@
 package two_pointers
 
+import "math"
+
 /* ----------------------- Two Sum II Array is sorted ----------------------- */
 
 // Time Complexity O(n)
@@ -23,4 +25,26 @@ func TwoSum(numbers []int, target int) []int {
 	}
 
 	return []int{left + 1, right + 1}
+}
+
+/* ----------------------- Container With Most Water ----------------------- */
+// Time Complexity O(n)
+// Space Complexity O(1)
+func MaxArea(height []int) int {
+	maximumArea := 0
+	left := 0
+	right := len(height) - 1
+
+	for left < right {
+		area := (right - left) * int(math.Min(float64(height[left]), float64(height[right])))
+		maximumArea = int(math.Max(float64(maximumArea), float64(area)))
+
+		if height[right] > height[left] {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return maximumArea
 }
