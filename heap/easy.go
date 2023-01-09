4b6e73853c9bc0c90fe041570b39a98db5aac8f1
Faris CHTATOU <faris.manage@gmail.com>
2023-01-09 01:14:19 +0100

    update heap kth largest element

--- heap/easy.go
+++ heap/easy.go
@@ -14,25 +14,32 @@ int add(int val) Appends the integer val to the stream and returns the element r
 // Time Complexity O(n)
 // Space Complexity O(n)
 type KthLargest struct {
-	h *IntHeap
+	h IntHeap
 	k int
 }
 
 func Constructor(k int, nums []int) KthLargest {
-	instance := KthLargest{h: (*IntHeap)(&nums), k: k}
-	heap.Init(instance.h)
-	for len(*instance.h) > k {
-		heap.Pop(instance.h)
+	minHeap := IntHeap{}
+	for _, value := range nums {
+		heap.Push(&minHeap, value)
+	}
+	instance := KthLargest{
+		h: minHeap,
+		k: k,
+	}
+	heap.Init(&instance.h)
+	for len(instance.h) > k {
+		heap.Pop(&instance.h)
 	}
 	return instance
 }
 
 func (kth *KthLargest) Add(val int) int {
-	heap.Push(kth.h, val)
+	heap.Push(&kth.h, val)
 
-	if len(*kth.h) > kth.k {
-		heap.Pop(kth.h)
+	if len(kth.h) > kth.k {
+		heap.Pop(&kth.h)
 	}
 
-	return (*kth.h)[0]
+	return (kth.h)[0]
 }
